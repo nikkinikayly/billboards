33d9bd2f1c325ebf168548d371cf874a76d287fd
Nikki Johnson <nikkijohnson@Nikkis-MacBook-Air.local>
2018-11-29 20:27:10 -0700

    changed controller and views for artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,51 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :update, :edit, :destroy]
+
   def index
+    @artists = Artist.all
   end
 
   def show
+
   end
 
   def new
+    @artist = Artist.new
+    render partial: "form"
   end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+        redirect_to artists_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render partial: "form"
+  end
+
+  def update
+    if @artist.update(artist_params)
+      redirect_to @artist
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to root_path
+  end
+
+  private
+    def artist_params
+      params.require(:artist).permit(:name, :genre)
+    end
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
 end
